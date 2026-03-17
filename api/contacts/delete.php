fda52204a60618fa24192a9da0cc8ff1594a689f
AghilesHCC <hadcenterconsulting@gmail.com>
2026-03-17 02:34:42 +0100

    Fix API contacts CORS by removing headers

--- api/contacts/delete.php
+++ api/contacts/delete.php
@@ -1,14 +1,6 @@
 <?php
 require_once __DIR__ . '/../bootstrap.php';
 
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: DELETE, OPTIONS');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
 $auth = new Auth();
 $userId = $auth->authenticate();
 
